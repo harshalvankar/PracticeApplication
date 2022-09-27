ff64b42621ea47ddf1da9c5c26825eaa2bb677b6
harshalinfo@gmail.com <harshalinfo@gmail.com>
2022-09-27 12:10:26 +0530

    Add project files.

---
+++ PracticeApplication/Base64Encrypter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeApplication
+{
+    public class Base64Encrypter
+    {
+        public static string Encrypt(string data)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(data);
+            var plainTextBytes1 = System.Text.Encoding.UTF8.GetBytes(Convert.ToBase64String(plainTextBytes));
+            var plainTextBytes2 = System.Text.Encoding.UTF8.GetBytes(Convert.ToBase64String(plainTextBytes1));
+
+            return Convert.ToBase64String(plainTextBytes2);
+        }
+        public static string Decrypt(string data)
+        {
+            var plainTextBytes = System.Convert.FromBase64String(data);
+            var plainTextBytes1 = System.Convert.FromBase64String(Encoding.UTF8.GetString(plainTextBytes));
+            var plainTextBytes2 = System.Convert.FromBase64String(Encoding.UTF8.GetString(plainTextBytes1));
+                       
+            return System.Text.Encoding.UTF8.GetString(plainTextBytes2);
+        }
+    }
+}
---
+++ PracticeApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace PracticeApplication
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = Guid.NewGuid().ToString();
+
+            string data = "b3HQPsY3UBi98nlZUqLCy9Z5WWppR0cO";
+
+            string data2 = Base64Encrypter.Encrypt(data);
+            string data3 = Base64Encrypter.Decrypt(data2);
+
+            Console.WriteLine(data);
+            Console.WriteLine(data2);
+            Console.WriteLine(data3);
+
+            //Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss"));
+            //Console.WriteLine(FormatDate(DateTime.Now));
+            //Console.WriteLine(System.Globalization.CultureInfo.CurrentCulture);
+            //Console.ReadLine();
+        }
+        public static string FormatDate(DateTime dateTime)
+        {
+            return string.Format(Convert.ToString(dateTime), "dd:MM:yyyy hh:MI:ss");
+        }
+    }
+}
